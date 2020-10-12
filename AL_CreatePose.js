475f95af4c5e58a9c1a8cfe3f5b0fcfc5accd194
Alarigger <47170756+alarigger@users.noreply.github.com>
2020-10-12 22:22:20 +0200

    check sub namespace
    Check namespace before duplicating else abort

--- AL_CreatePose.js
+++ AL_CreatePose.js
@@ -7,126 +7,153 @@ www.alarigger.com
 
 function AL_createPose(){
 
-/*Add a sub with the same name to several drawing in one click 
-*/
+	/*Add a sub with the same name to several drawing in one click 
+	*/
 
-/***************** V A R I A B L E S */
+	/***************** V A R I A B L E S */
 
-var selectedNodes = selection.numberOfNodesSelected(); 
+	var selectedNodes = selection.numberOfNodesSelected(); 
 
-var curFrame = frame.current();
+	var curFrame = frame.current();
 
-var POSE_NAME = "unamed_pose";
+	var POSE_NAME = "unamed_pose";
 
-var numSelLayers = Timeline.numLayerSel;
-
-
-/**************** E X E C U T I O N */
+	var numSelLayers = Timeline.numLayerSel;
 
 
-
-scene.beginUndoRedoAccum("AL_createPose");
-
-inputDialog()
+	/**************** E X E C U T I O N */
 
 
 
-scene.endUndoRedoAccum();
+	scene.beginUndoRedoAccum("AL_createPose");
+
+	inputDialog()
 
 
 
-/**************** F U N C T I O N S */ 
+	scene.endUndoRedoAccum();
 
 
-/*I N P U T   D I A L O G*/
 
-function inputDialog() {
-
-MessageLog.trace("inputDialog")
-
-   var d = new Dialog()
-   d.title = "Create_Pose";
-   d.width = 100;
-
-var nameInput = new LineEdit();
-nameInput.label = "Pose name : ";
-nameInput.maximum = 1000;
-nameInput.minimum = 1;
-d.add( nameInput );
+	/**************** F U N C T I O N S */ 
 
 
-if ( d.exec() ){
+	/*I N P U T   D I A L O G*/
 
- POSE_NAME = nameInput.text;
+	function inputDialog() {
 
- Add_new_subtitutions()
+		MessageLog.trace("inputDialog")
 
-}
+		   var d = new Dialog()
+		   d.title = "Create_Pose";
+		   d.width = 100;
 
-
-}
-
-function Add_new_subtitutions(){
-
-MessageLog.trace("\n===============Add_new_subtitutions")
-
-for ( var i = 0; i < numSelLayers; i++ ){
-
-if ( Timeline.selIsColumn(i)){
-
-var currentColumn = Timeline.selToColumn(i);
-MessageLog.trace(currentColumn );
-
-if (column.type(currentColumn) == "DRAWING"){
-
-	column.duplicateDrawingAt (currentColumn, curFrame)	
-
-	var DrawingName =column.getDrawingName(currentColumn,curFrame)
-	var split0=DrawingName.split('-');
-
-	var split1=split0[1];
-
-	if(split1 != "" &&typeof(split1) == "string"){
-	var split2 = split1.split('.')
-	var  oldTiming= split2[0];
-
-	/*var timing = column.getDrawingTimings(currentColumn);
-	MessageLog.trace( timing );*/
-
-	var newTiming = POSE_NAME; 
+		var nameInput = new LineEdit();
+		nameInput.label = "Pose name : ";
+		nameInput.maximum = 1000;
+		nameInput.minimum = 1;
+		d.add( nameInput );
 
 
-	var renameOperation = column.renameDrawing(currentColumn, oldTiming,newTiming )	
+		if ( d.exec() ){
+
+		 POSE_NAME = nameInput.text;
+
+		 Add_new_subtitutions()
+
+		}
 
 
-	MessageLog.trace( DrawingName );
-	MessageLog.trace("oldTiming : "+oldTiming);
-	MessageLog.trace("NewTiming : "+newTiming);
-	MessageLog.trace("drawing renamed :"+ renameOperation);
+	}
+	
+	function Extract_drawingName(tvgname){
+		var split0=tvgname.split('-');
+
+		var split1=split0[1];
+		
+		var result = "";
+
+		if(split1 != "" &&typeof(split1) == "string"){
+			var split2 = split1.split('.')
+			var  oldTiming= split2[0];	
+			result = oldTiming;
+		}
+		return result;
+	}
+
+	function Add_new_subtitutions(){
+
+		MessageLog.trace("\n===============Add_new_subtitutions")
+
+		for ( var i = 0; i < numSelLayers; i++ ){
+
+			if ( Timeline.selIsColumn(i)){
+
+				var currentColumn = Timeline.selToColumn(i);
+				MessageLog.trace(currentColumn );
+
+				if (column.type(currentColumn) == "DRAWING"){
+
+					
+					
+					var sub_timing = column.getDrawingTimings(currentColumn);
+					var number_of_subs = sub_timing.length;
+					
+					for(var s=0; s<number_of_subs ;s++){
+
+						
+						var sub_name = Extract_drawingName(sub_timing[s])
+						MessageLog.trace("subname : "+sub_timing[s]);
+						MessageLog.trace("posename : "+POSE_NAME);
+						
+						if(sub_timing[s] == POSE_NAME){
+							MessageBox.warning("the pose name already exist in the selected drawings");
+							return;
+						}
+						MessageLog.trace("no match");
+
+					}
+					
+					column.duplicateDrawingAt (currentColumn, curFrame)	
+					
+					var DrawingName =column.getDrawingName(currentColumn,curFrame)
+	
+
+					var oldTiming= Extract_drawingName(DrawingName)
+
+					var newTiming = POSE_NAME; 
 
 
-		if(renameOperation!==true){
+					var renameOperation = column.renameDrawing(currentColumn, oldTiming,newTiming )	
 
-				MessageBox.warning("La pose n'a pas pu être créée, Verifier que pose ayant le mem nom n'existe pas déjà");
 
-				break;
+					MessageLog.trace( DrawingName );
+					MessageLog.trace("oldTiming : "+oldTiming);
+					MessageLog.trace("NewTiming : "+newTiming);
+					MessageLog.trace("drawing renamed :"+ renameOperation);
+
+
+						/*if(renameOperation!==true){
+
+								MessageBox.warning("La pose n'a pas pu être créée, Verifier que pose ayant le mem nom n'existe pas déjà");
+
+								break;
+
+						}*/
+
+					
+
+
+
+
+				}
+
+				MessageLog.trace("\n===============Add_new_subtitutions____")
+
+			}
 
 		}
 
 	}
-
-
-
-
-}
-
-}
-}
-
-MessageLog.trace("\n===============Add_new_subtitutions____")
-
-}
-
-
 
 }
