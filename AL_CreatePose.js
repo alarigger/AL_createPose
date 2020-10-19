1c4988eb4acdff9baad55a6a161a8bb79904ceec
Alarigger <47170756+alarigger@users.noreply.github.com>
2020-10-19 22:15:37 +0200

    Update AL_CreatePose.js
    no bug with clones , now duplicate is an option, otherwise it juste rename it

--- AL_CreatePose.js
+++ AL_CreatePose.js
@@ -1,13 +1,13 @@
 /******************************** C R E A T E   P O S E  ***************************/
 /*
 Alexandre Cormier 
-29/06/2019
+19/10/2020
 www.alarigger.com
 */
 
 function AL_createPose(){
 
-	/*Add a sub with the same name to several drawing in one click 
+	/*Rename subs and duplicate if wanted. 
 	*/
 
 	/***************** V A R I A B L E S */
@@ -19,6 +19,12 @@ function AL_createPose(){
 	var POSE_NAME = "unamed_pose";
 
 	var numSelLayers = Timeline.numLayerSel;
+	
+	var DUPLICATE = false;
+	
+	var columns_to_treat = Array();
+	
+	var return_message ="";
 
 
 	/**************** E X E C U T I O N */
@@ -53,13 +59,27 @@ function AL_createPose(){
 		nameInput.maximum = 1000;
 		nameInput.minimum = 1;
 		d.add( nameInput );
+		
+		var dupliInput = new CheckBox();
+		dupliInput.text = "duplicate sub ";
+		
+		d.add( dupliInput );
 
 
 		if ( d.exec() ){
 
 		 POSE_NAME = nameInput.text;
-
-		 Add_new_subtitutions()
+		 DUPLICATE = dupliInput.checked;
+		 
+		 if(fetch_and_check_columns()){
+				if(treat_columns()){
+					
+				}else{
+					MessageBox.warning(return_message);
+				}
+		 }else{
+				MessageBox.warning(return_message);
+		 }
 
 		}
 
@@ -67,6 +87,7 @@ function AL_createPose(){
 	}
 	
 	function Extract_drawingName(tvgname){
+		
 		var split0=tvgname.split('-');
 
 		var split1=split0[1];
@@ -81,79 +102,100 @@ function AL_createPose(){
 		return result;
 	}
 
-	function Add_new_subtitutions(){
-
-		MessageLog.trace("\n===============Add_new_subtitutions")
-
+	function fetch_and_check_columns(){
+		
 		for ( var i = 0; i < numSelLayers; i++ ){
 
 			if ( Timeline.selIsColumn(i)){
+				
+				MessageLog.trace(Timeline.selToNode(i));
 
 				var currentColumn = Timeline.selToColumn(i);
-				MessageLog.trace(currentColumn );
+				MessageLog.trace("currentColumn : "+currentColumn);
 
-				if (column.type(currentColumn) == "DRAWING"){
+				if (column.type(currentColumn) == "DRAWING" && columns_to_treat.indexOf(currentColumn)==-1){
 
-					
 					
 					var sub_timing = column.getDrawingTimings(currentColumn);
 					var number_of_subs = sub_timing.length;
 					
 					for(var s=0; s<number_of_subs ;s++){
-
 						
 						var sub_name = Extract_drawingName(sub_timing[s])
-						MessageLog.trace("subname : "+sub_timing[s]);
-						MessageLog.trace("posename : "+POSE_NAME);
 						
 						if(sub_timing[s] == POSE_NAME){
-							MessageBox.warning("the pose name already exist in the selected drawings");
-							return;
+
+							return_message = "the pose name already exist in the selected drawings";
+							return false;
+							
 						}
-						MessageLog.trace("no match");
 
 					}
 					
-					column.duplicateDrawingAt (currentColumn, curFrame)	
+					columns_to_treat.push(currentColumn);
 					
-					var DrawingName =column.getDrawingName(currentColumn,curFrame)
-	
-
-					var oldTiming= Extract_drawingName(DrawingName)
-
-					var newTiming = POSE_NAME; 
-
-
-					var renameOperation = column.renameDrawing(currentColumn, oldTiming,newTiming )	
-
-
-					MessageLog.trace( DrawingName );
-					MessageLog.trace("oldTiming : "+oldTiming);
-					MessageLog.trace("NewTiming : "+newTiming);
-					MessageLog.trace("drawing renamed :"+ renameOperation);
-
-
-						/*if(renameOperation!==true){
-
-								MessageBox.warning("La pose n'a pas pu être créée, Verifier que pose ayant le mem nom n'existe pas déjà");
-
-								break;
-
-						}*/
-
-					
-
-
-
-
 				}
-
-				MessageLog.trace("\n===============Add_new_subtitutions____")
+				
 
 			}
 
-		}
-
+		}		
+		
+		MessageLog.trace(columns_to_treat);
+		
+		return true
+		
 	}
+	
+	function check_input(char_input){
+			
+		
+		
+	}
+
+	function treat_columns(){
+
+		MessageLog.trace("\n===============Add_new_subtitutions")
+
+		for ( var i = 0; i < columns_to_treat.length; i++ ){
+
+				var currentColumn = columns_to_treat[i];
+				MessageLog.trace("currentColumn : "+currentColumn);
+
+				var sub_timing = column.getDrawingTimings(currentColumn);
+				
+				var number_of_subs = sub_timing.length;
+				
+				if(DUPLICATE){
+					column.duplicateDrawingAt (currentColumn, curFrame)	;
+						
+				}
+					
+				var DrawingName =column.getDrawingName(currentColumn,curFrame)
+	
+				var oldTiming= Extract_drawingName(DrawingName)
+
+				var newTiming = POSE_NAME; 
+					
+	
+
+				if(column.renameDrawing(currentColumn, oldTiming,newTiming )) {
+
+				}else{
+					return_message = "unauthorised character in the pose name";
+				}
+				
+				
+
+		}
+		
+		return true;
+		
+	}
+			
+		
+
+		
+
 
 }
